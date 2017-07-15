2243f3b514cc8279fa80d5112467d321be9473d7
Wiega Sonora <wiegasonora@gmail.com>
2017-07-15 22:38:34 +0700

    add some activity

--- TaNiaga/app/src/main/java/cidoeq/taniaga/Login.java
+++ TaNiaga/app/src/main/java/cidoeq/taniaga/Login.java
@@ -1,13 +1,20 @@
 package cidoeq.taniaga;
 
+import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Login extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Window windowLogin = getWindow();
+        windowLogin.setStatusBarColor(Color.rgb(0, 102, 61));
     }
 }
---
+++ TaNiaga/app/src/main/java/cidoeq/taniaga/Splashscreen.java
@@ -0,0 +1,46 @@
+package cidoeq.taniaga;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Splashscreen extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+
+        Window windowLogin = getWindow();
+        windowLogin.setStatusBarColor(Color.DKGRAY);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splashscreen.this, Category.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
