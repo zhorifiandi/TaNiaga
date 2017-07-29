4959a6c5eff2c8156332d4d1dbdf3c1cbe6eb59e
Wiega Sonora <wiegasonora@gmail.com>
2017-07-30 00:22:55 +0700

    add all remaining pages, perhaps it's finished

---
+++ TaNiaga/app/src/main/java/cidoeq/taniaga/SoldItem.java
@@ -0,0 +1,13 @@
+package cidoeq.taniaga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoldItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sold_item);
+    }
+}
