991297020c685ac956aa65079b5cabaf0b95687d
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-07-30 22:16:55 +0700

    downgrade

--- api-server/db/schema.rb
+++ api-server/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,71 +13,77 @@
 
 ActiveRecord::Schema.define(version: 20170730082309) do
 
-  create_table "farmers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "name"
-    t.string   "phone_number"
-    t.integer  "seller_id"
-    t.string   "array_item"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "incoming_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "agent_phone_number"
-    t.string   "from_number"
-    t.string   "list_of_contents"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+  create_table "farmers", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.string   "phone_number", limit: 255
+    t.integer  "seller_id",    limit: 4
+    t.string   "array_item",   limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "name"
+  create_table "incoming_messages", force: :cascade do |t|
+    t.string   "agent_phone_number", limit: 255
+    t.string   "from_number",        limit: 255
+    t.string   "list_of_contents",   limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name",         limit: 255
     t.text     "description",  limit: 65535
-    t.string   "category"
-    t.integer  "price"
-    t.string   "variety"
-    t.string   "dimension"
-    t.string   "seller_email"
+    t.string   "category",     limit: 255
+    t.integer  "price",        limit: 4
+    t.string   "variety",      limit: 255
+    t.string   "dimension",    limit: 255
+    t.string   "seller_email", limit: 255
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
 
-  create_table "outcoming_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "agent_phone_number"
-    t.string   "to_number"
-    t.string   "list_of_contents"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+  create_table "outcoming_messages", force: :cascade do |t|
+    t.string   "agent_phone_number", limit: 255
+    t.string   "to_number",          limit: 255
+    t.string   "list_of_contents",   limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
-  create_table "sellers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "address"
-    t.string   "phone_number"
-    t.integer  "user_id"
-    t.string   "array_farmer"
-    t.string   "array_item"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "sellers", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.string   "email",        limit: 255
+    t.string   "address",      limit: 255
+    t.string   "phone_number", limit: 255
+    t.integer  "user_id",      limit: 4
+    t.string   "array_farmer", limit: 255
+    t.string   "array_item",   limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "sms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "sms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "transactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "transactions", force: :cascade do |t|
     t.datetime "date"
-    t.integer  "item_id"
-    t.integer  "seller_id"
-    t.integer  "buyer_id"
-    t.string   "item_name"
-    t.string   "seller_name"
-    t.string   "buyer_name"
-    t.integer  "amount_price"
+    t.integer  "item_id",          limit: 4
+    t.integer  "seller_id",        limit: 4
+    t.integer  "buyer_id",         limit: 4
+    t.string   "item_name",        limit: 255
+    t.string   "seller_name",      limit: 255
+    t.string   "buyer_name",       limit: 255
+    t.integer  "amount_price",     limit: 4
     t.text     "description",      limit: 65535
-    t.string   "transaction_code"
+    t.string   "transaction_code", limit: 255
     t.boolean  "ispaid"
     t.boolean  "issent"
     t.boolean  "isreceived"
@@ -85,14 +92,14 @@ ActiveRecord::Schema.define(version: 20170730082309) do
     t.datetime "updated_at",                     null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "address"
-    t.string   "phone_number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.string   "address",         limit: 255
+    t.string   "phone_number",    limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
